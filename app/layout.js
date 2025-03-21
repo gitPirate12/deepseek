9bb223136f9a0a2494673cc22efaf4e6d8df1068
gitPirate12 <aneeqshaffy7@gmail.com>
2025-03-21 23:09:56 +0530

    Created appcontext provider for user context

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -16,6 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
+      <AppContextProvider>
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
@@ -23,6 +25,7 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </AppContextProvider>
     </ClerkProvider>
   );
 }
