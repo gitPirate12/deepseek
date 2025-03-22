630d0473349463287492f1f3f4115302b009e2b7
gitPirate12 <aneeqshaffy7@gmail.com>
2025-03-22 08:31:13 +0530

    Created user model

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose || {conn: null, promise: null};
+let cached = global.mongoose || { conn: null, promise: null };
 
 export default async function connectDB() {
-
-  if(cached.conn) {
+  if (cached.conn) {
     return cached.conn;
   }
 
-  if(!cached.promise) {
-     cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose
-    );
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI)
+      .then((mongoose) => mongoose);
   }
   try {
     cached.conn = await cached.promise;
   } catch (error) {
-    console.error('Error connecting to database:', error);
+    console.error("Error connecting to database:", error);
   }
   return cached.conn;
 }
